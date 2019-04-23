342f21f4e3876ebfa0afce60e93a33911ba1ddf7
Josh Jaffe <josjaf@gmail.com>
2019-04-22 20:45:49 -0400

    attempting for python package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='newport_helpers',
+    version='0.1dev',
+    packages=['stacker_custom','helpers'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
