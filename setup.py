6a17ec383549e3194930fc7cff80c51491d5925a
Josh Jaffe <josjaf@gmail.com>
2019-04-22 22:56:36 -0400

    adding submodules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='newport_helpers',
     version='0.1dev',
-    packages=['stacker_custom','helpers'],
+    packages=['stacker_custom','helpers', 'stacker_custom/destroy_hooks', 'stacker_custom/hooks', 'stacker_custom/lookups'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     #url='https://gitlab.com/josjaf/newport_helpers/',
