d192da348b2e599f1b956ed8d608ce6bb62fd020
Josh Jaffe <josjaf@gmail.com>
2019-05-02 21:30:59 -0400

    updating constants for prod stage

--- newport_helpers/constants.py
+++ newport_helpers/constants.py
@@ -29,5 +29,19 @@ class Constants():
             'region': 'us-east-1',
             'env_file': f'conf/774906637191-security-app-pipeline.env',
             'config_file': 'security_app_pipeline.yml'
+        },
+        '088746295286-artifact-pipeline': {
+            'account_id': '088746295286',
+            'profile': 'shared',
+            'region': 'us-east-1',
+            'env_file': f'conf/088746295286-artifact-pipeline.env',
+            'config_file': 'artifact_pipeline.yml'
+        },
+        '088746295286-security-app-pipeline': {
+            'account_id': '088746295286',
+            'profile': 'shared',
+            'region': 'us-east-1',
+            'env_file': f'conf/088746295286-security-app-pipeline.env',
+            'config_file': 'security_app_pipeline.yml'
         }
     }
