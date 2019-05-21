97fbfe1eace5b5208f9ba66b984e23acd9ac71a0
Josh Jaffe <josjaf@amazon.com>
2019-05-20 22:17:38 -0400

    dynamically generating paths

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from distutils.core import setup
 import glob
 
+static = ['newport_helpers']
+packages = packages = glob.glob('stacker_custom/*/')
+final_packages = []
+for package in packages:
+    if '__pycache__' in package: continue
+    final_packages.append(package)
+final = static + final_packages
 setup(
     name='newport_helpers',
     version='0.1dev',
-    packages=['stacker_custom', 'newport_helpers', 'stacker_custom/destroy_hooks', 'stacker_custom/hooks',
-              'stacker_custom/lookups', 'stacker_custom/hooks/post', 'stacker_custom/stacker_blueprints'],
+    packages=final,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     author='josjaf',
     author_email='josjaf@gmail.com',
     install_requires=[
-        'boto3==1.9.134',
-        'botocore==1.12.134',
-        'jmespath== 0.9.4'
+        'boto3>=1.9.134',
+        'botocore>=1.12.134',
+        'jmespath>= 0.9.4'
     ],
     # url='https://gitlab.com/josjaf/newport_helpers/',
 )
