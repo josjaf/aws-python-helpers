c68067f970d5871b27f81fc3c86479d7670d00fa
Josh Jaffe <josjaf@gmail.com>
2019-05-23 22:15:23 -0400

    adding git helpers

---
+++ newport_helpers/git_helpers.py
@@ -0,0 +1,24 @@
+import git
+
+
+class Git_Helpers():
+
+    def get_commit_sha(self, dir='.', repo_bare_exception=False):
+        """
+
+        :param dir:
+        :param repo_bare_exception:
+        :return:
+        """
+        repo = git.repo.Repo(dir)
+        if repo.bare:
+            commit = None
+
+            if repo_bare_exception:
+                # when i get a blank repo, and repo_bare_exception=True
+                raise RuntimeError("Blank Git Repo")
+            else:
+                return commit
+
+        commit = repo.head.commit.hexsh
+        return commit
