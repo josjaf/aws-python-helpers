fbe75321a08602516709177fbb1d15ede72c2391
Josh Jaffe <josjaf@gmail.com>
2019-06-01 17:20:01 -0400

    adding ecr push

--- stacker_custom/hooks/post/docker_build_push.py
+++ stacker_custom/hooks/post/docker_build_push.py
@@ -56,33 +56,15 @@ def handler(context, provider, **kwargs):
 
     labels = {'Maintainer': 'josjaf', 'commit': sha, 'buildtime': buildtime}
     s.Docker_Helpers.docker_build_image(tag, docker_file, labels)
-
-
-
     session = boto3.session.Session()
-    region = session.region_name
-    identity = session.client("sts").get_caller_identity()
-    account_id = identity["Account"]
-    registry = f"{account_id}.dkr.ecr.{region}.amazonaws.com/{ecr_name}"
-
-    # tag image
-    docker_client = docker.from_env()
-    image = docker_client.images.get(f'{tag}:latest')
-    image.tag(registry, 'latest')
+    s.Docker_Helpers.ecr_push(session=session,tag=tag)
 
 
-    # login
-    ecr = session.client('ecr', region_name=session.region_name)
-    login_response = ecr.get_authorization_token()
-    token = b64decode(login_response['authorizationData'][0]['authorizationToken']).decode()
-    username, password = token.split(':', 1)
-    print(username, password)
-    print(f"UserName: {username}")
-    print(f"Password: {password}")
-    response = docker_client.login(username=username, password=password, registry=registry, reauth=True, email=None)
-    print(response)
-    response = docker_client.images.push(registry, tag="latest", auth_config=dict(username=username, password=password))
-    print(response)
+
+
+
+
+
     return labels
 
 
