9a1ede962f64d4f030066e6717f863796b027156
Josh Jaffe <josjaf@gmail.com>
2019-07-10 12:51:20 -0400

    adding check to see if docker daemon is running to avoid strange exceptions

--- newport_helpers/docker_helpers.py
+++ newport_helpers/docker_helpers.py
@@ -1,8 +1,9 @@
 import os
 import datetime
 import git
-
+import urllib3
 import docker
+import requests
 from base64 import b64decode
 
 
@@ -10,6 +11,24 @@ class DockerHelpers():
     def __init__(self, *args, **kwargs):
         return
 
+    def docker_running_check(self, docker_client):
+        """
+        ensure that the docker engine is running. this method avoids harder exceptions
+        to debug that come from the docker engine not running, like file not found error
+        :param docker_client:
+        :return:
+        """
+        try:
+            docker_client.containers.list()
+        except requests.exceptions.ConnectionError as e:
+            print("Ensure that Docker Daemon is running")
+            print("Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?")
+            raise RuntimeWarning
+        except Exception as e:
+            print(type(e))
+            print(e)
+
+        return
     def docker_build_image(self, tag, docker_file, labels, path='.'):
         #TODO Determine whether change directory is required to the same dir as the dockerfile
         """
@@ -20,7 +39,7 @@ class DockerHelpers():
         :return:
         """
         docker_client = docker.from_env()
-
+        self.docker_running_check(docker_client)
         t = datetime.datetime.now()
         print("Rebuilding the Container")
         repo = git.Repo(search_parent_directories=True)
@@ -28,8 +47,14 @@ class DockerHelpers():
 
         # TODO add exception processing for requests.exceptions.ConnectionError when docker dameon is not running
         buildtime = t.strftime("%m-%d-%Y %H:%M:%S")
+        # build returns tuple, with image object and build logs
+        try:
+            response = docker_client.images.build(path=path, tag=tag, labels=labels, dockerfile=docker_file)
+            for line in response[1]:
+                print(line)
 
-        response = docker_client.images.build(path=path, tag=tag, labels=labels, dockerfile=docker_file)
+        except Exception as e:
+            print(e)
         container_build_time = datetime.datetime.now() - t
         print(f"Rebuilding the container took: {container_build_time}")
         print("$ docker run example")
@@ -50,6 +75,7 @@ class DockerHelpers():
 
         # tag image
         docker_client = docker.from_env()
+        self.docker_running_check(docker_client)
         image = docker_client.images.get(f'{tag}:latest')
         image.tag(registry, tag)
 
