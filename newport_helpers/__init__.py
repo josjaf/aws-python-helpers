fd3615a9f9369ca0a0902a0bee9549a1615a36fe
Josh Jaffe <josjaf@amazon.com>
2019-08-28 20:17:15 -0400

    removing the importable newport helpers from init so all the modules dependencies are not required, will need to fix on packages where helpers are being used

--- newport_helpers/__init__.py
+++ newport_helpers/__init__.py
@@ -1,18 +1,3 @@
-from newport_helpers import helpers, docker_helpers, cfn_helpers, iam_helpers, org_helpers, codebuild_helpers, aws_credential_helpers
 
 
-class NPH():
-    """
-    Super class for importing
-    """
 
-    def __init__(self, *args, **kwargs):
-        return
-
-    Helpers = helpers.Helpers()
-    Docker_Helpers = docker_helpers.DockerHelpers()
-    Cfn_Helpers = cfn_helpers.CfnHelpers()
-    Iam_Helpers = iam_helpers.IamHelpers()
-    Org_Helpers = org_helpers.Organization_Helpers()
-    CodeBuild_Helpers = codebuild_helpers.CodeBuildHelpers()
-    AWSCredentialHelpers = aws_credential_helpers.AWSCredentialHelpers()
