d6015946ab21f40e236b4d151ce67790fa3fcef7
Josh Jaffe <josjaf@gmail.com>
2019-10-28 16:52:54 -0400

    adding ec2 helpers

---
+++ newport_helpers/ec2_helpers.py
@@ -0,0 +1,25 @@
+class EC2Helpers():
+
+
+    def __init__(self, *args, **kwargs):
+        return
+
+    def get_service_azs(self, session, service_name):
+        """
+        get the azs for a specific service
+        :param session:
+        :param service_name:
+        :return:
+        """
+        ec2 = session.client('ec2')
+        region = session.region_name
+        dns_service_name = f'com.amazonaws.{region}.{service_name}'
+        response = ec2.describe_vpc_endpoint_services(ServiceNames=[dns_service_name])
+        print(response)
+
+        # get the dictionary for the endpoint we are looking for
+        endpoint_config = \
+        [d for d in response['ServiceDetails'] if dns_service_name in d['ServiceName']][0]
+        availability_zones = sorted(endpoint_config['AvailabilityZones'])
+
+        return availability_zones
