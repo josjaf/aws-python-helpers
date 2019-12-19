338381ca5e86ec9959c5198091330c933771bc26
Josh Jaffe <josjaf@gmail.com>
2019-12-19 11:37:59 -0500

    updating kms helpers with more helpers

--- newport_helpers/kms_helpers.py
+++ newport_helpers/kms_helpers.py
@@ -1,4 +1,4 @@
-import boto3 
+import boto3
 
 
 class KMShelpers():
@@ -41,8 +41,45 @@ class KMShelpers():
         return alias_names
 
     def describe_key_from_alias(self, session, alias):
+        if not alias.startswith('alias/'):
+            print("alias must start with alias/")
         kms_client = session.client('kms')
         kms_response = kms_client.describe_key(KeyId=alias)
         return kms_response['KeyMetadata']['KeyId']
 
+    def list_grants_paginate(self, session, key_id):
+        grants_list = []
+        kms_client = session.client('kms')
+        paginator = kms_client.get_paginator('list_grants')
+        response_iterator = paginator.paginate(KeyId=key_id)
+        for page in response_iterator:
+            for item in page.get('Grants'):
+                grants_list.append(item)
+        return grants_list
 
+    def revoke_grants_by_name(self, session, key_id, grant_friendly_id):
+        kms_client = session.client('kms')
+        grants_list = self.list_grants_paginate(session, key_id)
+        for grant in grants_list:
+            if grant['Name'] == grant_friendly_id:
+                response = kms_client.revoke_grant(
+                    KeyId=grant['KeyId'],
+                    GrantId=grant['GrantId']
+                )
+        return response
+
+    def create_idempontent_grant(self, session, grant_friendly_id, grantee_arn, alias):
+        kms_client = session.client('kms')
+        kms_response = kms_client.describe_key(KeyId=f"alias/{alias}")
+        key_id = kms_response['KeyMetadata']['Arn']
+        grants_list = self.list_grants_paginate(session, key_id)
+        response = self.revoke_grants_by_name(session, key_id, grant_friendly_id)
+
+        response = kms_client.create_grant(
+            KeyId=key_id,
+            GranteePrincipal=grantee_arn,
+            # RetiringPrincipal='string',
+            Operations=['Decrypt', 'Encrypt', 'GenerateDataKey'],
+            Name=grant_friendly_id
+        )
+        return response
