3d7d81eb7c264e6ee3b08787c038228031be7522
Josh Jaffe <josjaf@gmail.com>
2019-12-24 18:57:41 -0500

    updating org helpers

--- newport_helpers/org_helpers.py
+++ newport_helpers/org_helpers.py
@@ -1,6 +1,7 @@
 import boto3
 from newport_helpers import helpers
 import threading
+from botocore.exceptions import ClientError
 Helpers = helpers.Helpers()
 
 
@@ -25,6 +26,7 @@ class Organization_Helpers():
         if remove_org_master:
             account_ids.remove(org_master_account_id)
         return account_ids
+
     def get_account_email_from_organizations(self, org_session, account_id):
         """
         pass in org session and account id and return the email associated with the account
@@ -41,6 +43,23 @@ class Organization_Helpers():
         account_id_dict = [a for a in response['Accounts'] if a['Id'] == account_id]
         account_email = account_id_dict[0]['Email']
         return account_email
+
+    def get_principal_org_id(self, session):
+        try:
+            organizations = session.client('organizations')
+            response = organizations.describe_organization()
+
+            principal_org_id = response['Organization']['Id']
+
+            params = {"PrincipalOrgID": principal_org_id}
+            return principal_org_id
+
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'AWSOrganizationsNotInUseException':
+                print("#" * 75)
+                raise RuntimeError("CREATE A NEW ORGANIZATION IN ACCOUNT OR JOIN TO CONTINUE")
+
+        return
 
     def org_loop_entry(self, org_profile=None, account_role=None):
         """
@@ -62,7 +81,7 @@ class Organization_Helpers():
     def org_loop_entry_thread_worker(self, account, account_role, session, results):
 
         session = Helpers.get_child_session(account, account_role, session)
-        response = (account,session)
+        response = (account, session)
         results.append(response)
 
     def org_loop_entry_thread(self, org_profile=None, account_role=None, remove_org_master=False):
@@ -84,7 +103,7 @@ class Organization_Helpers():
         def worker(account, session, results):
 
             session = Helpers.get_child_session(account, account_role, None)
-            response = (account,session)
+            response = (account, session)
             results.append(response)
 
         org_accounts = self.get_org_accounts(session, remove_org_master)
@@ -92,9 +111,9 @@ class Organization_Helpers():
         for account in org_accounts:
             t = threading.Thread(target=self.org_loop_entry_thread_worker,
                                  args=(account, account_role, session, results))
-            #t = threading.Thread(target=worker, args=(account, session, results))
+            # t = threading.Thread(target=worker, args=(account, session, results))
             threads.append(t)
-            print(f"Account {account}" )
+            print(f"Account {account}")
             t.start()
 
         print(len(threads))
@@ -102,7 +121,6 @@ class Organization_Helpers():
             thread.join()
         print(results)
         return results
-
 
 
 if __name__ == '__main__':
