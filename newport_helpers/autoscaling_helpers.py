1e7114f4bad471cb52504b0649cecb9a8338027e
Josh Jaffe <josjaf@gmail.com>
2020-05-11 10:07:50 -0400

    autoscaling helpers

--- newport_helpers/autoscaling_helpers.py
+++ newport_helpers/autoscaling_helpers.py
@@ -29,3 +29,14 @@ class AutoScalingHelpers():
         else:
             print(f"AutoScalingGroup: {asg_group_name} HAS lifecycle hooks")
             return True
+    def get_asg_from_name(self, session, asg_group_name):
+        """
+        describe asg from name
+        :param session:
+        :param asg_group_name:
+        :return:
+        """
+        asg = session.client('autoscaling')
+        response = asg.describe_auto_scaling_groups()
+        asg_response = [g for g in response['AutoScalingGroups'] if g['AutoScalingGroupName'] == asg_group_name][0]
+        return asg_response
