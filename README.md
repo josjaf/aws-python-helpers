b99d14ec351a9853135cc2d8547dfb37e7d9849b
Josh Jaffe <josjaf@gmail.com>
2020-09-01 17:41:40 -0400

    github prep

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description
+This is a pip install package of the helpers that I use with my projects
+
+
+# Installation
+`pip install git+https://github.com/josjaf/aws-python-helpers.git`
--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ import glob
 setup(
     name='newport_helpers',
     version='0.103',
-    packages=['stacker_custom', 'newport_helpers/', 'stacker_custom/destroy_hooks', 'stacker_custom/hooks',
-              'stacker_custom/lookups', 'stacker_custom/hooks/post', 'stacker_custom/stacker_blueprints',
+    packages=['newport_helpers/',
               ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
