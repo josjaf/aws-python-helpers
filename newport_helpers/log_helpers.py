5e1763cc3ffb460e956ee99f21f9083a657898e0
Josh Jaffe <josjaf@amazon.com>
2021-01-16 17:12:26 -0500

    updating log helpers

--- newport_helpers/log_helpers.py
+++ newport_helpers/log_helpers.py
@@ -10,10 +10,34 @@ def get_logger():
     log_level = str(os.environ.get('LOG_LEVEL')).upper()
     if log_level not in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
         log_level = 'INFO'
-    logger = logging.getLogger()
-    # logging.basicConfig(level=logging.DEBUG)
-    logger.setLevel(log_level)
+
+    log_format ='%(asctime)s %(message)s'
+    log_format = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    print(f"Log Level: {log_level}")
+    # logger.setLevel(level=log_level)
+    # logger = logging.getLogger()
     logger = logging.getLogger(__name__)
+
+    logging.getLogger().setLevel(level=log_level)
+
+    if logging.getLogger().hasHandlers():
+        # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
+        # `.basicConfig` does not execute. Thus we set the level directly.
+        logging.getLogger().setLevel(level=log_level)
+    else:
+        logging.basicConfig(level=log_level, format=log_format, datefmt=datefmt)
+        # logging.basicConfig(level=log_level)
+
+
+        # logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+    #### Supress botocore logs
+    logging.getLogger('botocore').setLevel(logging.WARNING)
+
+
     return logger
 
 
@@ -44,3 +68,8 @@ def get_logger_v21():
     logger = logging.getLogger()
 
     return logger
+
+if __name__ == '__main__':
+    logger = get_logger()
+    print(f"Log Level: {logger.level}")
+    logger.info("INFO")
